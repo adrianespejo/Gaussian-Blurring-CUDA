c01d7a2d3fba62377f5680b19d636cace5a11787
Raúl García <32136106+gfuentes97@users.noreply.github.com>
2019-05-04 17:07:04 +0200

    some improvements

--- main.cpp
+++ main.cpp
@@ -5,35 +5,51 @@
 #include "stb_image_write.h"
 
 
+#include <stdio.h>
 #include <iostream>
 #include <string>
+
 using namespace std;
+
+string root = "/home/raul/Dropbox/uni/Cuarto/TGA/PRACTICA/Gaussian-Blurring-CUDA/images/";
+//string root = "/home/bscuser/Documents/Gaussian-Blurring-CUDA/images/";
+
 
 struct pixel {
     int r, g, b;
 };
 
+unsigned char* LOAD(const string& imageName, int *width, int *height, int *comp, int desired_channels){
+
+    string imagePath = root + imageName;
+    char path[imagePath.length() + 1];
+    strcpy(path, imagePath.c_str());
+    return stbi_load(path, width, height, comp, desired_channels);
+}
+
+void WRITE(const string& imageName, int width, int height, int comp, const void *data, int quality){
+    string imagePath = root + imageName + ".jpg";
+    char path2[imagePath.length() + 1];
+    strcpy(path2, imagePath.c_str());
+    stbi_write_jpg(path2, width, height, comp, data, quality);
+}
+
 int main(int argc, char *argv[]) {
+
     int width, height, comp;
-    unsigned char *image = stbi_load("/home/bscuser/Documents/Gaussian-Blurring-CUDA/images/fruits.png", &width, &height, &comp, STBI_rgb);
+    string imageName = "fruits.png";
+    unsigned char *image = LOAD(imageName, &width, &height, &comp, STBI_rgb);
 
     if (image == nullptr) {
-        string message = "Failed to load texture.";
-        throw(message);
+        throw std::runtime_error("ERROR loading: " + root + imageName);
     }
-
-    unsigned char *new_image;
 
     for (int i = 0; i < width*height; i+=3) {
         int r = image[i] - '0';
         int g = image[i+1] - '0';
         int b = image[i+2] - '0';
-
-
     }
 
-// escribir
-    stbi_write_jpg("/home/bscuser/Documents/Gaussian-Blurring-CUDA/images/res2.jpg", width, height, STBI_rgb, image, 255);
-
+    WRITE("result",width, height, STBI_rgb, image, 255);
 
 }
