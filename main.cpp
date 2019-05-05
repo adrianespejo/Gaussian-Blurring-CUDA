0bb6e1f7addc52f028401129afb1af7c40af05e5
adrianespejo <adri.espejo@gmail.com>
2019-05-05 20:53:28 +0200

    iterando bien pero mal el filtrado

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include <string.h>
 
 using namespace std;
 
@@ -19,16 +18,20 @@ using namespace std;
 string root = "C:\\Users\\adrie\\OneDrive\\Documentos\\UNI\\TGA\\proyecto\\Gaussian-Blurring-CUDA\\images\\";
 
 
-struct pixel_t {
+struct pixel_int_t {
     int r, g, b;
 };
 
+struct pixel_double_t {
+    double r, g, b;
+};
 
-int kernel[5][5] = {{1 / 273, 4 / 273,  7 / 273,  4 / 273,  1 / 273},
-                    {4 / 273, 16 / 273, 26 / 273, 16 / 273, 4 / 273},
-                    {7 / 273, 26 / 273, 41 / 273, 26 / 273, 7 / 273},
-                    {4 / 273, 16 / 273, 26 / 273, 16 / 273, 4 / 273},
-                    {1 / 273, 4 / 273,  7 / 273,  4 / 273,  1 / 273}};
+
+double kernel[5][5] = {{1.0 / 273.0, 4.0 / 273.0,  7.0 / 273.0,  4.0 / 273.0,  1.0 / 273.0},
+                       {4.0 / 273.0, 16.0 / 273.0, 26.0 / 273.0, 16.0 / 273.0, 4.0 / 273.0},
+                       {7.0 / 273.0, 26.0 / 273.0, 41.0 / 273.0, 26.0 / 273.0, 7.0 / 273.0},
+                       {4.0 / 273.0, 16.0 / 273.0, 26.0 / 273.0, 16.0 / 273.0, 4.0 / 273.0},
+                       {1.0 / 273.0, 4.0 / 273.0,  7.0 / 273.0,  4.0 / 273.0,  1.0 / 273.0}};
 
 unsigned char *LOAD(const string &imageName, int *width, int *height, int *comp, int desired_channels) {
     string imagePath = root + imageName;
@@ -54,36 +57,46 @@ int main(int argc, char *argv[]) {
         throw std::runtime_error("ERROR loading: " + root + imageName);
     }
 
-    auto *new_image = (unsigned char *) malloc(width * height * sizeof(unsigned char));
+    auto *new_image = (unsigned char *) malloc(width * height * 3 * sizeof(unsigned char));
 
-    int npixels = 0;
+    int npixels, npixelsi, npixelsj;
+    npixels = npixelsi = npixelsj = 0;
     // one loop
 //    for (int i = 0; i < width*height*3; i++){
 //        new_image[i] = image[i];
 //        npixels++;
 //    }
-
+    width = width * 3;
     // two loops
-    for (int i = 0; i < height * 3; i++) {
+    for (int i = 0; i < height; i++) {
+        npixelsi++;
+        npixelsj = 0;
         for (int j = 0; j < width; j += 3) {
+            npixelsj++;
+            npixels++;
 //          dos for
-//            pixel_t pixel = {.r = image[i * height + j] - '0',
-//                    .g = image[i * height + j + 1] - '0',
-//                    .b = image[i * height + j + 2] - '0'};
-
             // i*height + j is the center of the 5*5 submatrix
-            pixel_t submatrix[5][5];
+            pixel_int_t submatrix[5][5];
+            int p, q;
+            p = q = 0;
             for (int i_b = i - 2; i_b <= i + 2; i_b++) {
-                for (int j_b = j - 2; j_b <= j + 2; j_b++) {
-                    pixel_t pixel_b = {.r = image[i_b * height + j_b] - '0',
-                            .g = image[i_b * height + j_b + 1] - '0',
-                            .b = image[i_b * height + j_b + 2] - '0'};
-                    submatrix[i_b][j_b] = pixel_b;
+                for (int j_b = j - 2; j_b <= j + 2; j_b += 3) {
+                    pixel_int_t pixel_b;
+                    if (i_b < 0 || j_b < 0 || i_b >= height || j_b >= width) {
+                        pixel_b = {.r = 0, .g = 0, .b = 0};
+                    } else {
+                        pixel_b = {.r = (int) image[i_b * width + j_b],
+                                .g = (int) image[i_b * width + j_b + 1],
+                                .b = (int) image[i_b * width + j_b + 2]};
+                    }
+                    submatrix[p][q] = pixel_b;
+                    q++;
                 }
+                p++;
             }
 
             // compute submatrix * kernel
-            pixel_t res[5][5];
+            pixel_double_t res[5][5];
             for (int m = 0; m < 5; m++) {
                 for (int n = 0; n < 5; n++) {
                     res[m][n] = {.r = 0, .g = 0, .b = 0};
@@ -94,22 +107,35 @@ int main(int argc, char *argv[]) {
                     }
                 }
             }
+            pixel_double_t sum = {.r = 0, .g = 0, .b = 0};
+            for (auto &row : res) {
+                for (auto &pos : row) {
+                    sum.r += pos.r;
+                    sum.g += pos.g;
+                    sum.b += pos.b;
+                }
+            }
 
-            // a continuacion hay que "sumar", no acabo de entender si se suman todos los valores de res y ese
-            // es el pixel i*height+j o si se suma cada posicion de res a cada pixel de la submatrix 5*5 de new_image
+            new_image[i * width + j] = (char) round(sum.r);
+            new_image[i * width + j + 1] = (char) round(sum.g);
+            new_image[i * width + j + 2] = (char) round(sum.b);
 
-//            new_image[i * height + j] = pixel.r + '0';
-//            new_image[i * height + j + 1] = pixel.g + '0';
-//            new_image[i * height + j + 2] = pixel.b + '0';
-//            npixels++;
+//            pixel_int_t pixel = {.r = (int) image[i * width + j],
+//                    .g = (int) image[i * width + j + 1],
+//                    .b = (int) image[i * width + j + 2]};
+//
+//            new_image[i * width + j] = (char) pixel.r;
+//            new_image[i * width + j + 1] = (char) pixel.g;
+//            new_image[i * width + j + 2] = (char) pixel.b;
         }
+        cout << "npixelsj: " << npixelsj << endl;
     }
 
     cout << "npixels: " << npixels << endl;
-    WRITE("result", width, height, STBI_rgb, image, 255);
-    WRITE("result_pruebas5", width, height, STBI_rgb, new_image, 255);
+    cout << "npixelsi: " << npixelsi << endl;
+    WRITE("result", width / 3, height, STBI_rgb, image, 255);
+    WRITE("blurred_fruits15", width / 3, height, STBI_rgb, new_image, 255);
 
     free(image);
     free(new_image);
-    return 0;
 }
